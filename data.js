8b0fec8f9e8c95e6976a3179e5e38c59f6904486
DoesntSee <95162271+DoesntSee@users.noreply.github.com>
2022-06-19 15:35:45 +1000

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 var album = {
-    title: "Test",
+    title: "Photo Album",
 
     collections: [
         {
-            name: "Screenshot 2022-06-19 1.13.35 PM.png",
+            name: "Test photos",
             photos: [
                 { caption: "Totally the Wii Shop", url: "https://raw.githubusercontent.com/DoesntSee/photoalbum/main/Screenshot%202022-06-19%201.13.35%20PM.png" }
             ]
